6cff8adde7d6eef011341a5fdca25b29c4fcc95f
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-19 20:50:41 +1000

    update subscribe home

--- pet_clone/src/components/SubscribeHome.js
+++ pet_clone/src/components/SubscribeHome.js
@@ -3,7 +3,7 @@ import SubscribeForm from "./SubscribeForm"
 const SubscribeHome = (props) => {
     return (
         <div>
-            <h1>Subscribe</h1>
+            <h1>Subscribe to keep</h1>
             <h6>To be the first to know about exciting promos & ways to clone your pet!</h6>
             <SubscribeForm />
             <p><a href="#">Privacy Policy</a></p>
