80fa17ef1ffa0bec5d8849d1d282f121871f6903
Eliza Nicholls <eliza@Elizas-MacBook-Pro.local>
2022-05-19 13:50:22 +1000

    componenents_basic

--- pet_clone/src/components/App.js
+++ pet_clone/src/components/App.js
@@ -1,9 +1,13 @@
-
+import SubscribeForm from "./SubscribeForm";
 
 function App() {
   return (
     <div className="App">
       Page coming soon.
+      {/* <DiscountBanner/> */}
+      <SubscribeForm/>
+  
+
     </div>
   );
 }
---
+++ pet_clone/src/components/SubscribeForm.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+const SubscribeForm = () => {
+    const [user, setUser] = useState({
+        first_name: '',
+        last_name: '',
+        email: ''
+    });
+
+    _handleChange = (event) => {
+        setUser((prevStates) => ({
+            [event.target.name]: event.target.value
+        }))
+    };
+
+    _handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(event);
+    }
+    
+    return (
+         <div>
+             <form onSubmit={_handleSubmit}>
+                 
+                 <input name="first_name" type="text" placeholder="First Name" required onChange={_handleChange} />
+                 <input name="last_name" type="text" placeholder="Last Name" required onChange={_handleChange} />
+                 <input name="email" type="email" placeholder="Email Address" required onChange={_handleChange} />
+
+
+                 <button>Subscribe</button>
+             </form>
+
+         </div>
+    )
+
+}
+
+export default SubscribeForm;
