d827d495e5c7e2415269fa99c662c7bb3069a8b7
Jaque Elizandro <jaque@elizandro.com.br>
2022-05-20 10:27:24 +1000

    style changes

--- pet_clone/src/components/Heading.js
+++ pet_clone/src/components/Heading.js
@@ -3,9 +3,9 @@ import Heading1 from "../heading1.svg"
 
 const Heading = (props) => {
     return (
-        <div className="heading-image">
-            <img src={Heading1} width="500" height="500" />
-            <div className="heading">
+        <div className="heading">
+            <img src={Heading1}  />
+            <div className="heading-text">
                 <h1 className="heading-title">WHY NOT have TWO?</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. A eget amet ut ac.</p>
                 <button className="btn">Discover more</button>
--- pet_clone/src/components/Navigation.js
+++ pet_clone/src/components/Navigation.js
@@ -4,9 +4,9 @@ import UserLogo from "../user-logo.svg"
 const Navigation = (props) => {
     return (
         <nav className="nav">
-            <img src={PetLogo} width="60" height="60" />
+            <img src={PetLogo} className="logo" />
             <h1>My Pet 2.0</h1>
-            <img className="userlogo" src={UserLogo} width="60" height="60" />
+            <img className="userlogo" src={UserLogo} />
         </nav>
     );
   }
